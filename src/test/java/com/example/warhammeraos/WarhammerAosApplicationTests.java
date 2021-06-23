ce487a93d5c9fd06bb61b66d550aa05db8997c74
Tim Bauer <timbauer@ymail.com>
2021-06-23 10:59:38 -0700

    initial commit

---
+++ src/test/java/com/example/warhammeraos/WarhammerAosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warhammeraos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarhammerAosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
